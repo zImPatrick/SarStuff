6c863aa5c5384b813f1e9a6efcf05ccbaaf0e427
zImPatrick <23613354+zImPatrick@users.noreply.github.com>
2024-08-02 20:05:55 +0200

    fix: SarExtract broken in release mode

--- SarExtract/Program.cs
+++ SarExtract/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 
-using System.Diagnostics;
 using System.Text;
 
 namespace SarExtract
@@ -10,35 +9,52 @@ namespace SarExtract
         static void Main(string[] args)
         {
             BinaryReader reader = new BinaryReader(File.OpenRead("data.sar"));
-            Debug.Assert(reader.ReadBytes(6) != Encoding.ASCII.GetBytes("SARCFV"));
-            Debug.Assert(reader.ReadBytes(2) != new byte[] { 0x01, 0x01 });
+            if (Encoding.ASCII.GetString(reader.ReadBytes(6)) != "SARCFV")
+            {
+                throw new InvalidDataException("Data does not begin with SARCFV");
+            }
+
+            var dieseZweiBytes = reader.ReadBytes(2);
+            if (dieseZweiBytes.Equals(new byte[] { 0x01, 0x01 }))
+            {
+                // Ich bin mir nicht sicher, ob dass wirklich eine Version Number ist, aber wer weiß?
+                throw new InvalidDataException("Unsupported version?");
+            }
+            
 
             var tableEntries = reader.ReadInt32();
             var tableOffset = reader.ReadInt32();
 
             reader.BaseStream.Seek(tableOffset, SeekOrigin.Begin);
 
-            //string map = "";
+            string map = "";
 
+            Console.WriteLine($"Reading {tableEntries} entries");
             for (int i = 0; i < tableEntries; i++) {
                 var strLength = reader.ReadByte();
-                var fileName = Encoding.ASCII.GetString(reader.ReadBytes(strLength + 1));
+                var fileName = Encoding.ASCII.GetString(reader.ReadBytes(strLength));
+                // strLength hat danach noch \00
+                reader.ReadByte();
                 var offset = reader.ReadUInt32();
                 var length = reader.ReadUInt32();
                 reader.ReadBytes(4); // kp wofür diese bytes sind
 
+                Console.WriteLine($"Reading {fileName}");
+
                 var currentPosition = reader.BaseStream.Position;
 
                 var fileNameNormalized = Path.GetFileName(fileName);
-                //map += fileNameNormalized.Substring(0, fileNameNormalized.Length - 1) + ":" + fileName.Substring(0, fileName.Length - 1) + "\r\n";
-                var fs = File.OpenWrite(fileNameNormalized.Substring(0, fileNameNormalized.Length - 1));
+                map += fileNameNormalized + ":" + fileName + "\r\n";
+
+                var fs = File.OpenWrite(fileNameNormalized);
                 reader.BaseStream.Seek(offset, SeekOrigin.Begin);
                 fs.Write(reader.ReadBytes((int) length));
                 fs.Close();
+
                 reader.BaseStream.Seek(currentPosition, SeekOrigin.Begin);
             }
 
-            //File.WriteAllText("map.txt", map);
+            File.WriteAllText("map.txt", map);
 
         }
     }
